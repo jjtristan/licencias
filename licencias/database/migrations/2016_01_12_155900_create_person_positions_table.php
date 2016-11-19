c0452cad47c57979f14b50bd7a08182cf5965b2c
jjtristan <you@example.com>
2016-11-19 11:44:52 -0600

    Se cambian los seeders a inserts cuando se crean las tablas

--- licencias/database/migrations/2016_01_12_155900_create_person_positions_table.php
+++ licencias/database/migrations/2016_01_12_155900_create_person_positions_table.php
@@ -19,6 +19,21 @@ class CreatePersonPositionsTable extends Migration
 
             $table->timestamps();
         });
+        $now = date('Y-m-d H:i:s');
+        \DB::table('person_positions')->insert(
+            [
+                'name' => 'Arquitecto',
+                'created_at' => $now,
+            ],
+            [
+                'name' => 'Ingeniero',
+                'created_at' => $now,
+            ],
+            [
+                'name' => 'Técnico ambiental',
+                'created_at' => $now,
+            ]
+        );
     }
 
     /**
--- licencias/database/migrations/2016_01_14_125802_create_license_type_stages_table.php
+++ licencias/database/migrations/2016_01_14_125802_create_license_type_stages_table.php
@@ -37,6 +37,49 @@ class CreateLicenseTypeStagesTable extends Migration
 
             $table->timestamps();
         });
+        // Tipo 1 - Comunicado de Actividad
+        $stagesTypeOne = [
+            17,
+            18,
+            19,
+            20,
+            16
+        ];
+
+        $this->createStages($stagesTypeOne, 1);
+
+        // Tipo 2 Licencias Sin Calificación
+        $stagesTypeTwo = [
+            17,
+            18,
+            19,
+            20,
+            16
+        ];
+
+        $this->createStages($stagesTypeTwo, 2);
+
+        // Tipo 3 Licencias Con Calificación
+        $stagesTypeThree = [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16
+        ];
+
+        $this->createStages($stagesTypeThree, 3);
     }
 
     /**
@@ -47,5 +90,43 @@ class CreateLicenseTypeStagesTable extends Migration
     public function down()
     {
         Schema::drop('license_type_stages');
+    }
+
+    /**
+     * @param $stages
+     * @param $licenseType
+     */
+    private function createStages($stages, $licenseType) {
+        $weight = 0;
+        foreach ($stages as $stage) {
+            if ($weight != 0) {
+                $licenseTypeStagePrevious = CityBoard\Entities\LicenseTypeStage::where('license_type_id',
+                    $licenseType)->where('weight', $weight - 1)->first();
+            }
+            $licenseTypeStage = new CityBoard\Entities\LicenseTypeStage();
+            $licenseTypeStage->license_type_id = $licenseType;
+            $licenseTypeStage->license_stage_id = $stage;
+            $licenseTypeStage->weight = $weight;
+
+            $licenseTypeStage->previous = NULL;
+            if ($weight != 0) {
+                $licenseTypeStage->previous = $licenseTypeStagePrevious->license_stage_id;
+            }
+
+            $licenseTypeStage->next = NULL;
+            if ($weight != 0) {
+                $licenseTypeStagePrevious->next = $stage;
+                $licenseTypeStagePrevious->save();
+            }
+
+            $licenseTypeStage->license_generate = TRUE;
+            if ($weight != 0) {
+                $licenseTypeStagePrevious->license_generate = FALSE;
+                $licenseTypeStagePrevious->save();
+            }
+
+            $licenseTypeStage->save();
+            $weight++;
+        }
     }
 }
